5039d3019c1edaca633bff4cb8a8c6b3a1235b39
Danielle Midulla <danielle.midulla@gmail.com>
2017-04-24 17:22:57 -0400

    cleaning up the options.js file

--- options.js
+++ options.js
@@ -6,85 +6,41 @@ exports.sitemap = [
   {
     header: 'Category A',
     links: [
-      {
-        name: 'Link A.1',
-        url: '#',
-      }, {
-        name: 'Link A.2',
-        url: '#',
-      }, {
-        name: 'Link A.3',
-        url: '#',
-      }, {
-        name: 'Link A.4',
-        url: '#',
-      }, {
-        name: 'Link A.5',
-        url: '#',
-      }
+      { name: 'Link A.1', url: '#' },
+      { name: 'Link A.2', url: '#' },
+      { name: 'Link A.3', url: '#' },
+      { name: 'Link A.4', url: '#' },
+      { name: 'Link A.5', url: '#' }
     ],
   },
   {
     header: 'Category B',
     links: [
-      {
-        name: 'Link B.1',
-        url: '#',
-      }, {
-        name: 'Link B.2',
-        url: '#',
-      }, {
-        name: 'Link B.3',
-        url: '#',
-      }, {
-        name: 'Link B.4',
-        url: '#',
-      }, {
-        name: 'Link B.5',
-        url: '#',
-      }
+      { name: 'Link B.1', url: '#' },
+      { name: 'Link B.2', url: '#' },
+      { name: 'Link B.3', url: '#' },
+      { name: 'Link B.4', url: '#' },
+      { name: 'Link B.5', url: '#' }
     ],
   },
   {
     header: 'Category C',
     links: [
-      {
-        name: 'Link C.1',
-        url: '#',
-      }, {
-        name: 'Link C.2',
-        url: '#',
-      }, {
-        name: 'Link C.3',
-        url: '#',
-      }, {
-        name: 'Link C.4',
-        url: '#',
-      }, {
-        name: 'Link C.5',
-        url: '#',
-      }
+      { name: 'Link C.1', url: '#' },
+      { name: 'Link C.2', url: '#' },
+      { name: 'Link C.3', url: '#' },
+      { name: 'Link C.4', url: '#' },
+      { name: 'Link C.5', url: '#' }
     ],
   },
   {
     header: 'Category D',
     links: [
-      {
-        name: 'Link D.1',
-        url: '#',
-      }, {
-        name: 'Link D.2',
-        url: '#',
-      }, {
-        name: 'Link D.3',
-        url: '#',
-      }, {
-        name: 'Link D.4',
-        url: '#',
-      }, {
-        name: 'Link D.5',
-        url: '#',
-      }
+      { name: 'Link D.1', url: '#' },
+      { name: 'Link D.2', url: '#' },
+      { name: 'Link D.3', url: '#' },
+      { name: 'Link D.4', url: '#' },
+      { name: 'Link D.5', url: '#' }
     ],
   },
 ];
